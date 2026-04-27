33dc15427e58069e0a61845897ec0a02da0cdf87
Daniel <21williamson@parmiters.herts.sch.uk>
2026-04-27 10:41:26 +0100

    Add Problem 4

---
+++ Python/Problem4.py
@@ -0,0 +1,7 @@
+high_product = 0
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i*j
+        if str(product) == str(product)[::-1] and product > high_product:
+            high_product = product
+print(high_product)
